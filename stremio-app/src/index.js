8be02d6ce019ed02d8b2d94cbbf9833eceb9d332
Yasemin_ <yasemin.idrizova24@gmail.com>
2018-07-02 10:44:32 +0300

    create add/remove favorite movie function and finished html

--- stremio-app/src/index.js
+++ stremio-app/src/index.js
@@ -2,34 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './less_sandbox/css/style.css';
-import Stremio from './Stremio';
 import registerServiceWorker from './registerServiceWorker';
 
 
-function Elements(props){
-    return(
+function Elements(props) {
+
+   var toggleFunc = function (elemKey) {
+        var favoriteList = localStorage.getItem("favorite");
+
+        if (!favoriteList) {
+            favoriteList = [];
+            favoriteList.push(elemKey);
+            localStorage.setItem("favorite", JSON.stringify(favoriteList));
+        }
+        else {
+            favoriteList = JSON.parse(favoriteList);
+
+            if (favoriteList.indexOf(elemKey) == -1) {
+                favoriteList.push(elemKey);
+            } else {
+                favoriteList.splice(favoriteList.indexOf(elemKey), 1);
+            }
+
+            localStorage.setItem("favorite", JSON.stringify(favoriteList));
+        }
+    }
+
+    return (
         <div className="elements">
             <p>{props.movie.name}</p>
-            <img src={props.movie.poster}></img>
-            <img key={props.movie.imdb_id} className="toggle" src='https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_13-512.png'></img>
+            <img src={props.movie.poster}  alt=""></img>
+            <img key={props.movie.imdb_id} onClick={toggleFunc.bind(null, props.movie.imdb_id)} className="toggle" src='https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_13-512.png' alt=""></img>
         </div>
     )
 }
 
-
-function Rows(props){
-    return(
+function Rows(props) {
+    return (
         <div className="row">
-        <h2>{props.row.type}</h2>
-        {
-        props.row.response.metas.map(function(index) {
-           return  <Elements movie={index} />
-        })
-    }
+            <h2>{props.row.type}</h2>
+
+            {
+                props.row.response.metas.map(function (index) {
+                    return <Elements movie={index} />
+                })
+            }
         </div>
     );
 }
- 
+
+function Library(props) {
+    return (
+        <div>
+            {
+                props.all.map(function (index) {
+                    return <Rows row={index} />
+                })
+            }
+        </div>
+    );
+}
+
+
 const client = require('./stremio-addon-client')
 const officialAddons = require('./stremio-official-addons')
 const aggregators = require('./stremio-aggregators')
@@ -43,27 +77,21 @@ col.load(officialAddons)
 const aggr = new aggregators.Catalogs(col.addons)
 
 aggr.run()
-
-const dali =[];
-
 // Each time 'updated' is emitted you should re-fresh the view
 aggr.evs.on('updated', function() {
-	// iterate through aggr.results
-    // each result is a row of items that you have to display
-    console.log(aggr);
-    localStorage.setItem('testObject', JSON.stringify(aggr.results))
-	
 
-var dano =  JSON.parse(localStorage.getItem('testObject'));
-console.log(dano);
+var getArray =  aggr.results;
+getArray = getArray.slice(0, 6);
 
-    ReactDOM.render(<Rows row={dano[0]} />, document.getElementById('root'));
-    registerServiceWorker();
+for(var i = 0; i < getArray.length; i++){
+   getArray[i].response.metas = getArray[i].response.metas.slice(0,4);
+}
 
+
+ReactDOM.render(<Proba />, document.getElementById('root'));
+registerServiceWorker();
 
 });
-
-
 
 
 
